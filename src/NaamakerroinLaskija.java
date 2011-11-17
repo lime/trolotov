cb12194bea20ef09f3dca32d0f2236973a4ee985
Emil S <emil.sagfors@aalto.fi>
2011-11-17 15:50:57 +0200

    NaamaKerroinLaskija: Senjan muutokset

--- src/NaamakerroinLaskija.java
+++ src/NaamakerroinLaskija.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
  */
 
 /**
- * @author lime
+ * @author Senja
  * 
  */
 public class NaamakerroinLaskija {
@@ -15,7 +15,29 @@ public class NaamakerroinLaskija {
 	static {
 		naamakertoimet = new HashMap<String, Double>();
 
-		naamakertoimet.put("Buubbeli", 2.0);
+	}
+	
+	//TODO käytä silloin kun joinataan
+	/**
+	 * Metodia on tarkoitus käyttää, kun bottii liittyy kanavalle ja käy kaikki
+	 * nickit läpi
+	 * TAI silloin kun uusi irkkaaja liittyy kanavalle
+	 * TAI kun joku vaihtaa nickiä
+	 * @param nimi Sen irkkaajan nick, jolle luodaan naamakerroin
+	 */
+	public static void luoNaamakerroin(String nimi){
+		if(!naamakertoimet.containsKey(nimi)){
+			if(nimi.equals("anQ") || nimi.equals("mevi")){
+				naamakertoimet.put(nimi, 10.0);
+			}
+			else if(nimi.equals("Buubbeli")){
+				naamakertoimet.put(nimi, 2.0);
+			}
+			else{
+				double kerroin = Math.random()*10;
+				naamakertoimet.put(nimi, kerroin);
+			}
+		}
 	}
 
 	/**Kertoo käyttäjän naamakertoimen
@@ -28,6 +50,32 @@ public class NaamakerroinLaskija {
 		} else {
 			//TODO -1 tällä hetkellä
 			return -1;
+		}
+	}
+	
+	/**
+	 * Metodi korottaa irkkaajan naamakerrointa 0.1:lla
+	 * 
+	 * @param nimi Sen irkkaajan nick, jonka naamakerrointa halutaan korottaa
+	 */
+	public static void korotaNaamakerrointa(String nimi){
+		if(naamakertoimet.containsKey(nimi)){
+			double kerroin = naamakertoimet.get(nimi);
+			kerroin = kerroin + 0.1;
+			naamakertoimet.put(nimi, kerroin);
+		}
+	}
+	
+	/**
+	 * Metodi alentaa irkkaajan naamakerrointa 0.1:lla
+	 * 
+	 * @param nimi Sen irkkaajan nick, jonka naamakerrointa halutaan alentaa
+	 */
+	public void alennaNaamakerrointa(String nimi){
+		if(naamakertoimet.containsKey(nimi)){
+			double kerroin = naamakertoimet.get(nimi);
+			kerroin = kerroin - 0.1;
+			naamakertoimet.put(nimi, kerroin);
 		}
 	}
 
